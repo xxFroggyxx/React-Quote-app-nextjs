c92a99a47885a0e0512bbb96b3b5c7ee7725b581
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-06-08 00:09:34 +0200

    refactor: changed to camelCase

--- pages/api/quotes.js
+++ pages/api/quotes.js
@@ -1,28 +1,28 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      ID: 0,
-      Name: "Steve Jobs",
-      Quote:
+      id: 0,
+      name: "Steve Jobs",
+      quote:
         "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking.",
-      Date: "12/06/2005",
-      Img: "/Author1.png",
+      date: "12/06/2005",
+      img: "/Author1.png",
     },
     {
-      ID: 1,
-      Name: "Mark Twain",
-      Quote:
+      id: 1,
+      name: "Mark Twain",
+      quote:
         "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
-      Date: "Undeclared",
-      Img: "/Author2.jpg",
+      date: "Undeclared",
+      img: "/Author2.jpg",
     },
     {
-      ID: 2,
-      Name: "Johnny Cash",
-      Quote:
+      id: 2,
+      name: "Johnny Cash",
+      quote:
         "All your life, you will be faced with a choice. You can choose love or hate.. I choose love.",
-      Date: "Undeclared",
-      Img: "/Author3.jpg",
+      date: "Undeclared",
+      img: "/Author3.jpg",
     },
   ]);
 }
