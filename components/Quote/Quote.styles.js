3e306082d40fc5bf2bc9a5b1eeef8634efc87630
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-06-08 00:48:24 +0200

    feat: create final design

--- components/Quote/Quote.styles.js
+++ components/Quote/Quote.styles.js
@@ -1,14 +1,93 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
-export const Container = styled.div`
-  height: 335px;
-  width: 670px;
-  display: grid;
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+}`;
 
+export const QuoteContainer = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  height: 335px;
+  width: 670px;
+  display: grid;
+  grid-template-areas: "Image Text" "Reset Reset";
+  grid-template-columns: 1fr 1fr;
+  place-items: center;
+
+  @media only screen and (max-width: 768px) {
+    grid-template-areas: "Image" "Text" "Reset";
+    grid-template-columns: 100%;
+    max-width: 280px;
+  }
 `;
 
-export const StyledImage = styled.div``;
+export const QuoteImage = styled.div`
+  grid-area: Image;
+  user-select: none;
+  img {
+    border: 1px solid black !important;
+  }
+  :before {
+    content: '"';
+    margin-top: -70px;
+    margin-left: 20px;
+    font-size: 120px;
+    position: absolute;
+    font-family: "Secular One", sans-serif;
+    z-index: 1;
+  }
+  :after {
+    content: "";
+    width: 0;
+    height: 0;
+    position: absolute;
+    z-index: -1;
+    margin-left: -190px;
+    margin-top: 33px;
+    border-style: solid;
+    border-width: 0 0 200px 200px;
+    border-color: transparent transparent gold transparent;
+  }
+`;
+
+export const QuoteText = styled.div`
+  grid-area: Text;
+  user-select: none;
+  font-family: "Open Sans", sans-serif;
+  p {
+    font-weight: 300;
+    margin: 0.5rem 1rem;
+    &:nth-child(3) {
+      margin: 0.1rem 1rem;
+      color: rgba(0, 0, 0, 0.5);
+      font-size: 13px;
+    }
+  }
+  span {
+    margin: 0.1rem 1rem;
+    font-weight: bolder;
+  }
+`;
+
+export const QuoteReset = styled.div`
+  grid-area: Reset;
+  svg {
+    cursor: pointer;
+    font-size: 40px;
+    color: gold;
+    @media only screen and (max-width: 320px) {
+      padding-bottom: 20px;
+    }
+    :hover {
+      animation: ${rotate} 0.35s normal ease-in-out;
+    }
+  }
+`;
--- components/Quote/index.js
+++ components/Quote/index.js
@@ -1,19 +1,33 @@
 import useSWR from "swr";
+import { HiRefresh } from "react-icons/hi";
 import { Img } from "components/Img";
-import { Container, StyledImage } from "./Quote.styles";
+import {
+  QuoteContainer,
+  QuoteImage,
+  QuoteReset,
+  QuoteText,
+} from "./Quote.styles";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export const Quote = () => {
-  const { data, error } = useSWR("/api/quotes", fetcher);
+  const { data: localApi, error } = useSWR("/api/quotes", fetcher);
   if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+  if (!localApi) return <div>Loading...</div>;
 
   return (
-    <Container>
-      <StyledImage>
-        <Img src={data[0].Img} />
-      </StyledImage>
-    </Container>
+    <QuoteContainer>
+      <QuoteImage>
+        <Img src={localApi[0].img} />
+      </QuoteImage>
+      <QuoteText>
+        <p>{localApi[0].quote}</p>
+        <span>{localApi[0].name}</span>
+        <p>{localApi[0].date}</p>
+      </QuoteText>
+      <QuoteReset>
+        <HiRefresh />
+      </QuoteReset>
+    </QuoteContainer>
   );
 };
