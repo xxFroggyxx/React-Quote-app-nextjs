ed2edc1a3eababfa7699a65e51a99a6fab490e2b
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-06-08 18:51:29 +0200

    refactor: delete unnecessary component

--- components/Img/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Image from "next/image";
-
-export const Img = ({ src }) => {
-  return (
-    <Image src={src} alt={"Author of the quote"} width={242} height={224} />
-  );
-};
