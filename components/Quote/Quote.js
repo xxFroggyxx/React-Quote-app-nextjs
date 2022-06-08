16c351885b4f67c7539ca24f95074758e61a8817
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-06-08 18:51:49 +0200

    refactor: barrel file

---
+++ components/Quote/Quote.js
@@ -0,0 +1,37 @@
+import { useRef } from 'react';
+import Image from 'next/image';
+import { QuoteContainer, QuoteImage, QuoteReset, QuoteText, ResetIcon } from './Quote.styles';
+
+const createAnimate = (element) => {
+  return element.animate([{ transform: 'rotate(0deg)' }, { transform: 'rotate(-360deg)' }], {
+    duration: 300,
+  });
+};
+
+export const Quote = ({ image, quote, name, date, onRefreshClick }) => {
+  const resetIconRef = useRef(null);
+
+  const handleClick = () => {
+    if (resetIconRef.current) {
+      createAnimate(resetIconRef.current).play();
+    }
+
+    onRefreshClick();
+  };
+
+  return (
+    <QuoteContainer>
+      <QuoteImage>
+        <Image src={image} alt="Author of the quote" width={242} height={224} />
+      </QuoteImage>
+      <QuoteText>
+        <p>{quote}</p>
+        <span>{name}</span>
+        <p>{date}</p>
+      </QuoteText>
+      <QuoteReset ref={resetIconRef}>
+        <ResetIcon onClick={handleClick} />
+      </QuoteReset>
+    </QuoteContainer>
+  );
+};
